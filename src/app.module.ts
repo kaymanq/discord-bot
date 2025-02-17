2de02ca04d0dee93182f2199c151b68ba71ebb51
Kayman <ofmencall@gmail.com>
2025-02-17 20:58:13 +0300

    feat:add discord module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DiscordModule } from './discord/discord.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath:'.env',
     }),
+    DiscordModule,
   ],
 })
 export class AppModule {}
