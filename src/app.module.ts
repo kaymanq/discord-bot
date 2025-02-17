834ee1e89b5affb6267487cb6b105bf4548b0e78
Kayman <ofmencall@gmail.com>
2025-02-17 20:54:29 +0300

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DiscordModule } from './discord/discord.module';
 
 @Module({
   imports: [
@@ -8,7 +7,6 @@ import { DiscordModule } from './discord/discord.module';
       isGlobal: true,
       envFilePath:'.env',
     }),
-    DiscordModule.forRootAsync(),
   ],
 })
 export class AppModule {}
