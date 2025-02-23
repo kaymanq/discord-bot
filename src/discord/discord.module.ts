353afc8a6a92f2a5106d02215898d2857635ce44
Kayman <ofmencall@gmail.com>
2025-02-23 14:42:45 +0300

    feat: add static methods for discordModule

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,7 +1,44 @@
-import { Module } from '@nestjs/common';
-import { DiscordService } from './discord.service';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { Client, GatewayIntentBits } from 'discord.js';
 
-@Module({
-  providers: [DiscordService]
-})
-export class DiscordModule {}
+@Module({})
+export class DiscordModule {
+  static forRoot(options: { token: string; intents: GatewayIntentBits[] }): DynamicModule {
+    const discordProvider: Provider = {
+      provide: 'DISCORD_CLIENT',
+      useFactory: async () => {
+        const client = new Client({ intents: options.intents });
+        await client.login(options.token);
+        return client;
+      },
+    };
+
+    return {
+      module: DiscordModule,
+      providers: [discordProvider],
+      exports: [discordProvider],
+    };
+  }
+
+  static forRootAsync(options: {
+    useFactory: (...args: any[]) => Promise<{ token: string; intents: GatewayIntentBits[] }> | { token: string; intents: GatewayIntentBits[] };
+    inject?: any[];
+  }): DynamicModule {
+    const discordProvider: Provider = {
+      provide: 'DISCORD_CLIENT',
+      useFactory: async (...args: any[]) => {
+        const { token, intents } = await options.useFactory(...args);
+        const client = new Client({ intents });
+        await client.login(token);
+        return client;
+      },
+      inject: options.inject || [],
+    };
+
+    return {
+      module: DiscordModule,
+      providers: [discordProvider],
+      exports: [discordProvider],
+    };
+  }
+}
